f0b9acb1903ca2b135a6c64898e5b849c0590d0f
Totally-Kyle <kyle.nicholas95@hotmail.com>
2025-01-30 19:09:46 -0500

    daily ridership clean

---
+++ daily_ridership_clean.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#Load csv
+dr_riders = pd.read_csv("/Users/sa24/Documents/Phase 1 Project/Portfolio-Project/daily_ridership.csv")
+
+dr_riders.drop(["buses_total_estimated_ridersip","buses_of_comparable_pre_pandemic_day","lirr_total_estimated_ridership","lirr_of_comparable_pre_pandemic_day","metro_north_total_estimated_ridership","metro_north_of_comparable_pre_pandemic_day","access_a_ride_total_scheduled_trips","access_a_ride_of_comparable_pre_pandemic_day","bridges_and_tunnels_total_traffic","bridges_and_tunnels_of_comparable_pre_pandemic_day","staten_island_railway_total_estimated_ridership","staten_island_railway_of_comparable_pre_pandemic_day"], axis=1, inplace=True)
+
+dr_riders.to_csv("/Users/sa24/Documents/Phase 1 Project/Portfolio-Project/daily_ridership_clean.csv", index=False)
+
+print("CSV created")
