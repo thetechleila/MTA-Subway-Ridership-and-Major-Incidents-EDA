a22f000a57f55e85a5390aabef2c59491ed0bba0
SA10 <sa10@SA10s-MacBook-Air.local>
2025-02-07 23:31:20 -0500

    final

--- Data_transformation/merge.py
+++ Data_transformation/merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
+#open and read csv files
 df1 = pd.read_csv("combined_df.csv")
 df2 = pd.read_csv("incidents.csv")
-
+#merge them together
 merged_df = df1.merge(df2, on="date", how="inner")
 merged_df.to_csv("merged_file.csv", index=False)
